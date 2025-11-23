b5acb6a3899c13f84762d0dc4679bceff9750e64
yjj8353 <yjj8353@gmail.com>
2025-11-24 00:49:38 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <a-layout class="main-layout">
+    <!-- 사이드바 -->
+    <!-- 주의: sider의 width는 내부적으로 인라인 스타일로 지정되므로, style 속성으로 조정할 수 없음 -->
+    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible :width="siderWidth">
+      <div class="logo" />
+      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+        <a-menu-item key="1">
+          <user-outlined />
+          <span>nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <video-camera-outlined />
+          <span>nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <!-- 헤더 -->
+      <a-layout-header style="background: #fff; padding: 0">
+        <menu-unfold-outlined
+          v-if="collapsed"
+          class="trigger"
+          @click="() => (collapsed = !collapsed)"
+        />
+        <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
+      </a-layout-header>
+
+      <!-- 콘텐츠 -->
+      <a-layout-content :style="{ padding: '24px', background: '#fff', minHeight: '280px' }">
+        Content
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+} from '@ant-design/icons-vue'
+const selectedKeys = ref<string[]>(['1'])
+const collapsed = ref<boolean>(false)
+const siderWidth = ref<number>(250)
+</script>
+<style>
+.trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+.trigger:hover {
+  color: #1890ff;
+}
+
+.logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.3);
+  margin: 16px;
+}
+
+.site-layout .site-layout-background {
+  background: #fff;
+}
+
+.main-layout {
+  height: 100vh;
+}
+</style>
