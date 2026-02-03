e68abb63a1efa4a0416d593c0943037efadd5edd
yjj8353 <yjj8353@gmail.com>
2026-02-03 17:05:45 +0900

    클러스터 등록 페이지 변경

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -10,6 +10,8 @@ export const handlers = [
       { id: 5, parentId: 3, name: "외부망 클러스터", path: "/cluster/external", component: "pages/ClusterPage" },
       { id: 6, parentId: null, name: "로그인", path: "/login", component: "layouts/LoginLayout" },
       { id: 7, parentId: 6, name: "로그인", path: "", component: "pages/LoginPage" },
+      { id: 8, parentId: 1, name: "설정", path: "", component: "layouts/EmptyLayout" },
+      { id: 9, parentId: 8, name: "클러스터 등록", path: "/settings/cluster", component: "pages/ClusterRegistPage" },
     ]);
   }),
 ];
--- src/pages/ClusterRegistPage.vue
+++ src/pages/ClusterRegistPage.vue
@@ -11,18 +11,18 @@
     <n-row :gutter="24">
       <n-col span="12">
         <n-space vertical>
-          <n-input placeholder="클라우드" :v-model="cloud" />
-          <n-input placeholder="클러스터 이름" :v-model="clusterName" />
-          <n-input placeholder="연결주소" :v-model="ipAddr" />
-          <n-input placeholder="포트" :v-model="port" />
+          <n-input placeholder="클라우드" v-model:value="cloud" />
+          <n-input placeholder="클러스터 이름" v-model:value="clusterName" />
+          <n-input placeholder="연결주소" v-model:value="ipAddr" />
+          <n-input placeholder="포트" v-model:value="port" />
         </n-space>
       </n-col>
       <n-col span="12">
         <n-space vertical>
-          <n-input placeholder="계정" :v-model="id" />
-          <n-input placeholder="비밀번호" type="password" :v-model="password" />
+          <n-input placeholder="계정" v-model:value="id" />
+          <n-input placeholder="비밀번호" type="password" v-model:value="password" />
           <n-button id="test-button" type="primary"> 연결 테스트 </n-button>
-          <n-input placeholder="설명" type="textarea" :v-model="description" />
+          <n-input placeholder="설명" type="textarea" v-model:value="description" />
         </n-space>
       </n-col>
     </n-row>
@@ -30,14 +30,14 @@
     <n-row gutter="24">
       <n-col span="12">
         <n-space vertical>
-          <n-select placeholder="수집어댑터" />
+          <n-select v-model:value="collectionAdapterValue" :options="collectionAdapterOptions" />
         </n-space>
       </n-col>
       <n-col span="12">
         <n-row align="middle">
           <n-col span="4" class="text-center"> 수집간격 </n-col>
           <n-col span="18">
-            <n-input-number placeholder="수집간격" :v-model="collectionInterval" />
+            <n-input-number placeholder="수집간격" v-model:value="collectionInterval" />
           </n-col>
           <n-col span="2" class="text-center"> 분 </n-col>
         </n-row>
@@ -56,12 +56,17 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const cloud = ref(null);
-const clusterName = ref(null);
-const ipAddr = ref(null);
-const port = ref(null);
-const id = ref(null);
-const password = ref(null);
-const description = ref(null);
+const cloud = ref("");
+const clusterName = ref("");
+const ipAddr = ref("");
+const port = ref("");
+const id = ref("");
+const password = ref("");
+const description = ref("");
+const collectionAdapterValue = ref(null);
+const collectionAdapterOptions = ref([
+  { label: "Prometheus", value: "prometheus" },
+  { label: "Other", value: "other" },
+]);
 const collectionInterval = ref(1);
 </script>
