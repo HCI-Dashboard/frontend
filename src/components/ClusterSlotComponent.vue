01d059e109528155db338a42a2cb4f259598d9bd
yjj8353 <yjj8353@gmail.com>
2026-02-04 13:55:10 +0900

    메인화면 카드 제거

--- src/components/ClusterSlotComponent.vue
+++ src/components/ClusterSlotComponent.vue
@@ -1,58 +1,56 @@
 <template>
-  <n-card>
-    <n-row>
-      <h2>{{ props.title }}</h2>
-    </n-row>
-    <n-row gutter="12">
-      <n-col :span="12">
-        <DoughnutChartComponent
-          title="호스트 상태"
-          :data="hostData"
-          :labels="hostLabels"
-          :backgroundColor="hostBackgroundColor"
-        />
-      </n-col>
-      <n-col :span="12">
-        <DoughnutChartComponent
-          title="가상머신 상태"
-          :data="vmData"
-          :labels="vmLabels"
-          :backgroundColor="vmBackgroundColor"
-        />
-      </n-col>
-    </n-row>
-    <n-row>
-      <h2>이벤트</h2>
-    </n-row>
-    <n-row>
-      <n-table>
-        <thead>
-          <tr>
-            <th id="event-time">시간</th>
-            <th id="event-host">호스트</th>
-            <th id="event-content">내용</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>2024-06-01 12:00:00</td>
-            <td>host1</td>
-            <td>호스트가 정상적으로 작동합니다.</td>
-          </tr>
-          <tr>
-            <td>2024-06-01 12:00:00</td>
-            <td>host1</td>
-            <td>vm-1이 비정상적으로 종료되었습니다.</td>
-          </tr>
-          <tr>
-            <td>2024-06-01 12:00:00</td>
-            <td>host1</td>
-            <td>vm-1이 새로 추가되었습니다.</td>
-          </tr>
-        </tbody>
-      </n-table>
-    </n-row>
-  </n-card>
+  <n-row>
+    <h2>{{ props.title }}</h2>
+  </n-row>
+  <n-row gutter="12">
+    <n-col :span="12">
+      <DoughnutChartComponent
+        title="호스트 상태"
+        :data="hostData"
+        :labels="hostLabels"
+        :backgroundColor="hostBackgroundColor"
+      />
+    </n-col>
+    <n-col :span="12">
+      <DoughnutChartComponent
+        title="가상머신 상태"
+        :data="vmData"
+        :labels="vmLabels"
+        :backgroundColor="vmBackgroundColor"
+      />
+    </n-col>
+  </n-row>
+  <n-row>
+    <h2>이벤트</h2>
+  </n-row>
+  <n-row>
+    <n-table size="small">
+      <thead>
+        <tr>
+          <th id="event-time">시간</th>
+          <th id="event-host">호스트</th>
+          <th id="event-content">내용</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>2024-06-01 12:00:00</td>
+          <td>host1</td>
+          <td>호스트가 정상적으로 작동합니다.</td>
+        </tr>
+        <tr>
+          <td>2024-06-01 12:00:00</td>
+          <td>host1</td>
+          <td>vm-1이 비정상적으로 종료되었습니다.</td>
+        </tr>
+        <tr>
+          <td>2024-06-01 12:00:00</td>
+          <td>host1</td>
+          <td>vm-1이 새로 추가되었습니다.</td>
+        </tr>
+      </tbody>
+    </n-table>
+  </n-row>
 </template>
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
